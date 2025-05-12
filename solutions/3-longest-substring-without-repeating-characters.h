76ab5edd09f4c4dee0667b710369a7731db3f7df
sunlef <cleanblue@DESKTOP-HPPTJIG.>
2025-05-13 01:19:15 +0800

    finish 3-longest-substring-without-repeating-characters

---
+++ solutions/3-longest-substring-without-repeating-characters.h
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/longest-substring-without-repeating-characters
+class Solution {
+ public:
+  int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    std::map<char, int> count;
+
+    int ans = 0;
+    for (int x = 0, y = -1; x < n; count[s[x] - 'a'] -= 1, x++) {
+      while (y + 1 < n && count[s[y + 1] - 'a'] == 0) {
+        y += 1;
+        count[s[y] - 'a'] += 1;
+      }
+      ans = std::max(ans, y - x + 1);
+    }
+
+    return ans;
+  }
+};
